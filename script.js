023c53f99be255785f5dcbad92c7f9f630ad1e8f
이동우 <dongwooly@dong-uui-MacBookPro.local>
2024-06-03 01:11:04 +0900

    Commit before merging

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ document.addEventListener('touchmove', function(event) {
         event.preventDefault(); 
     }
 }, { passive: false });
+
+document.addEventListener('contextmenu', function(e) {
+    if (e.target.tagName === 'IMG') {
+        e.preventDefault();
+    }
+});
+
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(function() {
+        alert('계좌번호가 복사되었습니다. 감사합니다!');
+    }, function(err) {
+        console.error('복사에 실패했습니다.', err);
+    });
+}
