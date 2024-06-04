32f74f777d2209a0c8a217d4a063c0c43d96a66f
dongwooly <ldw9451@gmail.com>
2024-06-05 08:26:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -81,18 +81,6 @@ document.addEventListener('contextmenu', function(e) {
     }
 });
 
-// 두 손가락 확대 허용을 위한 터치 이벤트 처리
-document.addEventListener('touchmove', function(event) {
-    if (event.touches.length > 1) {
-        // 두 손가락 이상의 터치 이벤트는 허용
-        return;
-    }
-    // 그 외의 터치 이벤트는 막음
-    if (event.scale !== 1) {
-        event.preventDefault();
-    }
-}, { passive: false });
-
 function copyToClipboard(text) {
     navigator.clipboard.writeText(text).then(function() {
         alert('계좌번호가 복사되었습니다. 감사합니다!');
